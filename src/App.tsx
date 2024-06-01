d4c3c4bd684c7cc6bbbd3594ae46bf8af006dd73
ramilka <lilya1976.03.03@gmail.com>
2024-06-01 13:04:58 +0600

    Сделал два массива и объединил их; Добавил картинки в assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import './App.css'
+import {Menu} from './types';
+import {useState} from 'react';
+import drinkImage from 'assets/drink.png';
+import foodImage from 'assets/food.png';
 
-function App() {
+const App = () => {
+  const MENU: Menu[] = [
+    {name: 'Hamburger', price: 80, image: `${foodImage}`},
+    {name: 'Coffee', price: 70, image: `${drinkImage}`},
+    {name: 'CheeseBurger', price: 90, image: `${foodImage}`},
+    {name: 'Tea', price: 50, image: `${drinkImage}`},
+    {name: 'Fries', price: 45, image: `${foodImage}`},
+    {name: 'Cola', price: 40, image: `${drinkImage}`},
+  ];
+
+  const [menu] = useState([
+    {name: 'Hamburger', count: 0},
+    {name: 'Coffee', count: 0},
+    {name: 'CheeseBurger', count: 0},
+    {name: 'Tea', count: 0},
+    {name: 'Fries', count: 0},
+    {name: 'Cola', count: 0},
+  ]);
+
+  const menuList = menu.map((menuItem, index) => ({
+    ...menuItem, ...MENU[index]}));
+
   return (
-    <>
-    </>
+      <div className="row">
+        <div className="chosenItems"></div>
+        <div className="MenuList">
+          <h1 className="title">Add Items:</h1>
+        </div>
+      </div>
   )
-}
+};
 
 export default App
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Menu {
+  name: string;
+  price: number;
+  image: string;
+}
