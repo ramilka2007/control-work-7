a73dc0a09a0b28ec2de6580b9f2bf61a5ae0a8c8
ramilka <lilya1976.03.03@gmail.com>
2024-06-01 15:36:23 +0600

    Сделал кнопку удаления; добавил общую цену.

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,14 @@ const App = () => {
 
   const haveOrders = menu.filter((item) => item.count > 0)
 
+  const totalPrice:number = menuList.reduce((acc, value) => {
+    if (value.count === 0) {
+      return acc;
+    } else {
+      return acc + (value.price * value.count);
+    }
+  }, 0);
+
   const addItem = (name: string) => {
     setMenu((prevMenu) => {
       return prevMenu.map((item) => {
@@ -40,43 +48,61 @@ const App = () => {
     });
   };
 
+  const deleteItem = (name: string) => {
+    setMenu((prevMenu) => {
+      return prevMenu.map((item) => {
+        if (item.name === name) {
+          return {...item, count: item.count - 1};
+        }
+        return item;
+      });
+    });
+  };
+
   return (
-      <div className="row">
-        <div className="chosenItems">
-          <div className="title">
-            <h1>Order Details:</h1>
-          </div>
-          {haveOrders.length === 0 ?
-            <>
-              <h2>Order is empty!</h2> <br/>
-              <span>Please add some items</span>
-            </>
-            :
-            <>
-              <div className="addedItems">
-                {menuList.map((menuItem) => (
-                  <OrderDetails key={menuItem.price - 10} name={menuItem.name} price={menuItem.price} count={menuItem.count}/>
-                ))}
-              </div>
-            </>
-          }
+    <div className="row">
+      <div className="chosenItems">
+        <div className="title">
+          <h1>Order Details:</h1>
         </div>
-        <div className="MenuList">
-          <div className="title">
-            <h1>Add Items:</h1>
-          </div>
-          <div className="itemsRow">
-            {menuList.map((menuItem) => (
-              <MenuForChoose
-                key={menuItem.price + 10}
-                name={menuItem.name}
-                price={menuItem.price}
-                image={menuItem.image}
-                addItemClick={() => addItem(menuItem.name)}/>
-            ))};
-          </div>
+        {haveOrders.length === 0 ?
+          <>
+            <h2>Order is empty! <br/> Please add some items</h2>
+          </>
+          :
+          <>
+            <div className="addedItems">
+              {menuList.map((menuItem) => (
+                <OrderDetails
+                  key={menuItem.price - 10}
+                  name={menuItem.name}
+                  price={menuItem.price}
+                  count={menuItem.count}
+                  deleteItemClick={() => deleteItem(menuItem.name)}/>
+              ))}
+            </div>
+            <div className="totalPrice">
+              <strong>Total price: {totalPrice} KGS</strong>
+            </div>
+          </>
+        }
+      </div>
+      <div className="MenuList">
+        <div className="title">
+          <h1>Add Items:</h1>
+        </div>
+        <div className="itemsRow">
+          {menuList.map((menuItem) => (
+            <MenuForChoose
+              key={menuItem.price + 10}
+              name={menuItem.name}
+              price={menuItem.price}
+              image={menuItem.image}
+              addItemClick={() => addItem(menuItem.name)}/>
+          ))};
         </div>
       </div>
+    </div>
   )
 };
 
--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -4,9 +4,10 @@ interface OrderDetailsProps {
   name: string;
   price: number;
   count: number;
+  deleteItemClick: React.MouseEventHandler;
 }
 
-const OrderDetails: React.FC<OrderDetailsProps> = ({name, price, count}) => {
+const OrderDetails: React.FC<OrderDetailsProps> = ({name, price, count, deleteItemClick}) => {
   return (
     <>
       {count === 0 ?
@@ -22,7 +23,7 @@ const OrderDetails: React.FC<OrderDetailsProps> = ({name, price, count}) => {
           <div className="itemPrice">
             <strong>{count * price} KGS</strong>
           </div>
-          <button>
+          <button onClick={deleteItemClick}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" id="cross">
               <path
                 d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path>
